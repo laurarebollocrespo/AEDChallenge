26cb9b858f19a4af2f89948139532a9fad2beb19
laurarebollocrespo <laura.rebollo.crespo@office365.estudiantat.upc.edu>
2024-11-16 13:15:25 +0100

    Create parametres importants.py

---
+++ parametres importants.py
@@ -0,0 +1,17 @@
+    programming_skills: Dict[str, int]
+    experience_level: Literal["Beginner", "Intermediate", "Advanced"]
+    hackathons_done: int
+
+    # Interests, preferences and constraints
+    interests: List[str]
+    preferred_role: Literal[
+        "Analysis", "Visualization", "Development", "Design", "Don't know", "Don't care"
+    ]
+    objective: str
+    interest_in_challenges: List[str]
+    preferred_languages: List[str]
+    friend_registration: List[uuid.UUID]
+    preferred_team_size: int
+    availability: Dict[str, bool]
+
+  

58c57825a4888910afbc50efebb745c78e13d124
Roberto-Nsoni <roberto.lulenga.nsoni@estudiantat.upc.edu>
2024-11-16 17:27:13 +0100

    Update del clasificador

--- classificador.py
+++ classificador.py
@@ -1,18 +1,30 @@
 from transformers import pipeline
 
-# Crear el pipeline de clasificación "zero-shot"
-clasificador = pipeline("zero-shot-classification", model="typeform/distilbert-base-uncased-mnli")
+def classificador_ai(text: str) -> str:
+    # Crear el pipeline de clasificación "zero-shot"
+    clasificador = pipeline("zero-shot-classification", model="typeform/distilbert-base-uncased-mnli")
 
-# Definir las etiquetas de intenciones
-etiquetas = ["I want to socialize or meet new people", "I want to level up my programming skills", "I want to have fun and enjoy", "I want to win."]
+    # Definir las etiquetas de intenciones y su mapeo a etiquetas cortas
+    long_label = [
+        "I want to socialize or meet new people",
+        "I want to level up my programming skills",
+        "I want to have fun and enjoy",
+        "I want to win."
+    ]
 
-# Texto de ejemplo
-texto = "I'm all about vibin' with fellow datathoners! For me, this datathon is about making new friends and having an absolute blast. I'm excited to participate in as many events as I can, like workshops, mini-competitions, and even social activities. I want to explore new skills and techniques, but mostly, I want to connect with like-minded people and share laughter and memories. Let's do this!"
+    short_label = [
+        "socialize",
+        "learn",
+        "enjoy",
+        "win"
+    ]
 
-# Clasificar el texto según las etiquetas
-resultado = clasificador(texto, candidate_labels=etiquetas)
+    # Clasificar el texto según las etiquetas
+    resultado = clasificador(text, candidate_labels=long_label)
 
-# Mostrar el resultado
-print("Clasificación completa:")
-for label, score in zip(resultado['labels'], resultado['scores']):
-    print(f"- {label}: {score:.2f}")
+    # Encontrar la etiqueta con mayor puntaje y mapearla a la etiqueta corta
+    mejor_equivalencia = resultado['labels'][0]
+    indice = long_label.index(mejor_equivalencia)
+    short_label = long_label[indice]
+
+    return short_label

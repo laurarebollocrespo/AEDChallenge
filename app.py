2ec8de21a7192303df9a72cf7860c0f1fc8ab05b
nuriaelizondo <nuria.elizondo@estudiantat.upc.edu>
2024-11-16 20:46:02 +0100

    si

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 import streamlit as st
 import matplotlib.pyplot as plt
 import random
+import json
+import pandas as pd
+
+# Configuración de la página
+st.set_page_config(page_title="Generador de Grupos", layout="centered", page_icon="👤")
+
+with open('datathon_participants.json', 'r') as file:
+    datos = json.load(file)
+
 
 st.title("Creación de Equipos para la Datathon FME")
 st.write("Bienvenido a nuestra herramienta interactiva.")
@@ -8,8 +17,6 @@ st.write("Bienvenido a nuestra herramienta interactiva.")
 if st.button("Formar Equipos"):
     st.write("Procesando...")
     # Aquí llamarías a la lógica de tu backend para formar los equipos.
-
-    
 
 if st.button("¿Cuantos equipos hay de x integrantes?"):
     miembros = {'Dos': 10, 'Tres': 15, 'Cuatro': 7}
@@ -26,10 +33,10 @@ if st.button("¿Cuantos equipos hay de x integrantes?"):
     st.pyplot(fig)
 
 if st.button("Ver equipos"):
-    st.write("Esta es nuestra propuesta de equipos:")
+    st.write("**Esta es nuestra propuesta de equipos:**")
 
 if st.button("Análisis equipos"):
-    st.write("El número total de equipos es:")
+    st.write("**El número total de equipos es:**")
 
     data = {'Tryhards': 10, 'Pasarlo bien': 15, 'Hacer amigos': 7}
 
@@ -44,22 +51,42 @@ if st.button("Análisis equipos"):
 
     st.pyplot(fig)
 
+nombres = [persona['name'] for persona in datos]
 
-
-# Nombres que tengamos en la base de datos
-nombres = ['Carlos', 'Ana', 'Pedro', 'María', 'Juan', 'Sofia']
-
-# Por si quieres poner tu el nombre.
-nombre_input = st.text_input("Introduce un nombre para saber en qué equipo va:")
+# Generación de nombre aleatorio
+nombre_leer = st.text_input("Introduce un nombre para saber en qué equipo va:")
+if nombre_leer:
+    if nombre_leer in nombres:
+        st.session_state.nombre_input = nombre_leer
+    else:
+        st.write(f'{nombre_leer} no es miembro de ningún grupo.')
 
 if st.button("Generar nombre aleatorio"):
-    nombre_random = random.choice(nombres) 
-    nombre_input = nombre_random 
+    nombre_random = random.choice(nombres)
+    st.session_state.nombre_input = nombre_random
 
-if nombre_input:
-    st.write(f"Nombre seleccionado: {nombre_input}")
+df = pd.read_json("datathon_participants.json")
+
+if 'nombre_input' in st.session_state:
+    nombre = st.session_state.nombre_input
+
+    st.write(f"**Nombre seleccionado:** {nombre}")
     st.write()
-    st.write(f"{nombre_input} va en el equipo con:") # Falta relacionarlo con los grupos que hagamos.
+    st.write(f"**{nombre} va en el equipo con:**") # Falta relacionarlo con los grupos que hagamos.
+
+    # Botón para mostrar más detalles
+    if st.button("Mostrar más detalles"):
+        si = df.loc[df['name'] == nombre]
+        
+        edad = si['age'].iloc[0]
+        year_of_study = si['year_of_study'].iloc[0]
+        experience_level = si['experience_level'].iloc[0]
+        
+        # Mostrar los datos en Streamlit
+        st.write(f"**Edad:** {edad}")
+        st.write(f"**Año de estudio:** {year_of_study}")
+        st.write(f"**Nivel de experiencia:** {experience_level}")
+
 
 st.markdown(
     """
